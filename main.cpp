4255595fee24d9a7b64f212159c7d741d4691ae7
Luis Fer <pastillalc@gmail.com>
2019-03-26 11:51:09 -0400

    100

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // lines for faster reading
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
     return 0;
 }
+
--- uVA/ej100/Main.cpp
+++ uVA/ej100/Main.cpp
@@ -1,10 +1,46 @@
 //
 // Created by luisfer on 25/3/2019.
 //
-
+/*
+ * Ejercicio 100 uVA
+ */
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // lines for faster reading
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int i, j, maximociclo = 0;
+    while (cin >> i >> j) {
+        maximociclo=0;
+        int ogi=i,ogj=j;
+        if(i>j){
+            int aux=j;
+            j=i;
+            i=aux;
+        }
+        for (int k = i; k <= j; ++k) {
+            int numero = k;
+            int cont = 1;
+            while (numero > 1) {
+                if (numero % 2 == 0) {
+                    numero = numero / 2;
+                } else {
+                    numero = numero * 3 + 1;
+                }
+                cont++;
+            }
+            if (cont > maximociclo) {
+                maximociclo = cont;
+            }
+        }
+        cout << ogi << " " << ogj << " " << maximociclo << endl;
+    }
+
     return 0;
 }
+
