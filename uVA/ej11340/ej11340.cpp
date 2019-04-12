d73e89b393b9bbd17b3a0fb5b53ce8f1b10e1595
Luis Fer <pastillalc@gmail.com>
2019-04-12 14:46:10 -0400

    ejercicio 11340

---
+++ uVA/ej11340/ej11340.cpp
@@ -0,0 +1,49 @@
+//
+// Created by luisfer on 29/3/2019.
+//
+#include <bits/stdc++.h>
+#include <map>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    map<char, double> mapaPrecios;
+    map<char, double >::iterator it;
+    int casos;
+    cin >> casos;
+    while (casos--) {
+        int cantDeCaracteres=0, precio=0;
+        cin >> cantDeCaracteres;
+        for (int i = 0; i < cantDeCaracteres; ++i) {
+            char c;
+            int valor;
+            cin >> c;
+            cin >> valor;
+            mapaPrecios[c] = valor;
+        }
+        int cantLineas;
+        cin >> cantLineas;
+        cin.ignore();
+        for (int j = 0; j < cantLineas; ++j) {
+            string linea="";
+            getline(cin, linea);
+            for (int i = 0; i < linea.size(); i++) {
+                it = mapaPrecios.find(linea[i]);
+                if (it != mapaPrecios.end()) {
+                        precio += it->second;
+                }
+            }
+        }
+        double resto= (double)precio/100;
+
+        printf("%0.2lf$\n", resto);
+        mapaPrecios.clear();
+    }
+
+    return 0;
+}
+
+
--- uVA/ej573/ej573.cpp
+++ uVA/ej573/ej573.cpp
@@ -1,46 +1,46 @@
-    //
-    // Created by luisfer on 27/3/2019.
-    //
+//
+// Created by luisfer on 27/3/2019.
+//
 
-    #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-    using namespace std;
+using namespace std;
 
-    int main() {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        double h, u, d, f, PosicionActual = 0, PorcentajeCansancio;
-        cin >> h >> u >> d >> f;
-        while (h != 0) {
-            bool succes = false;
-            int dia = 1;
-            PosicionActual = u;
-            if (PosicionActual > h) {
-                cout << "success on day " << dia << "\n";
-            } else {
-                PosicionActual = PosicionActual - d;
-                PorcentajeCansancio = u * (f * 0.01);
-                u = u - PorcentajeCansancio;
-                while (PosicionActual >= 0 && PosicionActual <= h) {
-                    dia++;
-                    PosicionActual = PosicionActual + u;
-                    if (PosicionActual > h) {
-                        succes = true;
-                        break;
-                    }
-                    PosicionActual = PosicionActual - d;
-                    u = u - PorcentajeCansancio;
-                    if(u<0){
-                        u=0;
-                    }
+int main573() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    double h, u, d, f, PosicionActual = 0, PorcentajeCansancio;
+    cin >> h >> u >> d >> f;
+    while (h != 0) {
+        bool succes = false;
+        int dia = 1;
+        PosicionActual = u;
+        if (PosicionActual > h) {
+            cout << "success on day " << dia << "\n";
+        } else {
+            PosicionActual = PosicionActual - d;
+            PorcentajeCansancio = u * (f * 0.01);
+            u = u - PorcentajeCansancio;
+            while (PosicionActual >= 0 && PosicionActual <= h) {
+                dia++;
+                PosicionActual = PosicionActual + u;
+                if (PosicionActual > h) {
+                    succes = true;
+                    break;
                 }
-                if (succes) {
-                    cout << "success on day " << dia << "\n";
-                } else {
-                    cout << "failure on day " << dia << "\n";
+                PosicionActual = PosicionActual - d;
+                u = u - PorcentajeCansancio;
+                if (u < 0) {
+                    u = 0;
                 }
             }
-            cin >> h >> u >> d >> f;
+            if (succes) {
+                cout << "success on day " << dia << "\n";
+            } else {
+                cout << "failure on day " << dia << "\n";
+            }
         }
-        return 0;
+        cin >> h >> u >> d >> f;
     }
+    return 0;
+}
